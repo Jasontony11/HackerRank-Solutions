ab4d02fe4ba7be74c464da2c77e468efcadc2724
Jasontony11 <tonyjason406@gmail.com>
2025-05-20 12:02:24 +0530

    Q2: Java_Stdin_and_STdout_I

---
+++ Java_Stdin_and_STdout_I.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Java_Stdin_and_STdout_I {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int num1= sc.nextInt();
+        int num2= sc.nextInt();
+        int num3= sc.nextInt();
+        
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+    }
+}
