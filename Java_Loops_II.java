daf2b44b6d0889a472c8b5f5addce16b4939be9a
Jasontony11 <tonyjason406@gmail.com>
2025-05-20 14:42:26 +0530

    Q6: Java_Loops_II

---
+++ Java_Loops_II.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Java_Loops_II {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int q= sc.nextInt();
+        
+        
+        for(int i=0;i<q;i++){
+            int a= sc.nextInt();
+            int b= sc.nextInt();
+            int n= sc.nextInt();
+            int sum=a;
+            
+            for(int j=0;j<n;j++){
+                sum+= Math.pow(2, j)*b;
+                System.out.print(sum+" ");
+                
+            }
+            System.out.println();
+        }
+        
+    }
+}
