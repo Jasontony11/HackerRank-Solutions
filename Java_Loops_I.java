4cbd1ed700668b2261392e7ba09109cb4bafd544
Jasontony11 <tonyjason406@gmail.com>
2025-05-20 12:51:04 +0530

    Q5: Java_Loops_I

---
+++ Java_Loops_I.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Java_Loops_I {
+    public static void main(String[] args)  {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        for(int i=1;i<11;i++){
+            System.out.println(num+ " x "+i+" = "+num*i);
+        }
+    }
+}
