ac7e06862fa439c95eca77fdf915d765143e644d
Jasontony11 <tonyjason406@gmail.com>
2025-05-22 17:05:28 +0530

    Q25: Java_Subarray

---
+++ Java_Subarray.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Java_Subarray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();  
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int negativeCount = 0;
+
+        for (int start = 0; start < n; start++) {
+            int sum = 0;
+            for (int end = start; end < n; end++) {
+                sum += arr[end];
+                if (sum < 0) {
+                    negativeCount++;
+                }
+            }
+        }
+
+        System.out.println(negativeCount);
+    }
+}
